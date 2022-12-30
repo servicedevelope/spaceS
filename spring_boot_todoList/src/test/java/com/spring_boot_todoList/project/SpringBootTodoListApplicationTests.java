dd53eeda19d5f0453142cef7e77e36570a771fab
servicedevelope <uc.service.developer@gmail.com>
2022-12-30 21:56:22 +0900

    1

---
+++ spring_boot_todoList/src/test/java/com/spring_boot_todoList/project/SpringBootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_todoList.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
